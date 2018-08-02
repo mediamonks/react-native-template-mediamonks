d330a07c1459caa395c0c28e0b85a1a1c96051b0
Tom Atterton <toma@mediamonks.com>
2018-08-02 09:30:30 +0200

    Add files that need to be removed, remove react-native from plugins of eslintrc

--- scripts/removeSetupFiles.js
+++ scripts/removeSetupFiles.js
@@ -7,6 +7,8 @@ const removeSetupFiles = () => {
   // Remove Files
   deletePath(path.resolve('scripts'));
   deletePath(path.resolve('App.js'));
+  deletePath(path.resolve('travis.yml'));
+  deletePath(path.resolve('LICENCE'));
   deletePath(path.resolve('readme.md'));
 };
 
